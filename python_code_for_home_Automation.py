1f8c5ec6a8e878b3dbea66c9eb90ad92afc4394b
notShahidKapoor <167243118+SnapTexas@users.noreply.github.com>
2025-07-25 18:10:24 +0530

    Update python_code_for_home_Automation.py

--- python_code_for_home_Automation.py
+++ python_code_for_home_Automation.py
@@ -1,20 +1,21 @@
 
-
+#required lib for mqtt communication 
 import paho.mqtt.client as mqtt
 import time
 
 # MQTT broker settings
-broker = "broker.hivemq.com"
+broker = "broker.hivemq.com"#Change this if not using hive mq which 
 port = 1883
-publish_topic_data = "HomeAutomationPrjctFostride/Esp/Pubdata"
+publish_topic_data = "Publishing topic for mqtt"
 
-subscribe_topic="HomeAutomationPrjctFostride/Esp/Subdata"
+subscribe_topic="Subscribing topic for mqtt"
 
 # Create MQTT client
 client = mqtt.Client()
 
 # Define callback for received messages
 def on_message(client, userdata, msg):
+    #writing the data we recieved to a file logs .txt if this file is not in the resent folder when downloading this code create a new file logs.txt in the folder being used
     with open('logs.txt','a') as f:
          f.write('\n')
          f.write(msg.payload.decode())
